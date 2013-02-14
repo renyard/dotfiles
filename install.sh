5cb4329686ecc995168fbb7ee5f1324f52ed1556
Ian Renyard <ian@renyard.net>
2013-02-14 00:09:35 +0000

    Now creates .bashrc and .vimrc if required. Fixed bug creating install directory.

--- install.sh
+++ install.sh
@@ -7,11 +7,11 @@ repo="https://github.com/renyard/dotfiles.git"
 
 
 # Prompt for install location.
-read -e -p "Install path: " -i "~/dotfiles" dt_dir
+read -e -p "Install path: " -i ~/dotfiles dt_dir
 
 # Create install location if it doesn't exist.
-if ! -d $dt_dir; then
-	echo 'Creating $dt_dir...'
+if [ ! -d $dt_dir ]; then
+	echo "Creating $dt_dir..."
 	mkdir -p $dt_dir
 fi
 
@@ -28,15 +28,31 @@ git submodule update
 
 # Update .*rc files.
 
-echo 'Linking .bashrc...'
 # .bashrc
+
+# Create .bashrc, if required.
+if [ ! -e ~/.bashrc ]; then
+	echo 'Creating ~/.bashrc...'
+	touch ~/.bashrc
+fi
+
+# Add source lines to .bashrc
+echo 'Linking .bashrc...'
 bashrc="if [ -f $dt_dir ]; then . $dt_dir/bashrc fi";
 if ! grep -Fxq "$bashrc" ~/.bashrc; then
 	echo $bashrc >> ~/.bashrc
 fi
 
-echo 'Linking .vimrc...'
 # .vimrc
+
+# Create .vimrc, if required.
+if [ ! -e ~/.vimrc ]; then
+	echo 'Creating ~/.vimrc...'
+	touch ~/.vimrc
+fi
+
+# Add source lines to .vimrc.
+echo 'Linking .vimrc...'
 vimrc="source $dt_dir/vimrc"
 if ! grep -Fxq "$vimrc" ~/.vimrc; then
 	echo $vimrc >> ~/.vimrc
