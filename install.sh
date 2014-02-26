354a41436534201184c5f7b1180e576e86937691
Ian Renyard <ian@renyard.net>
2014-02-26 23:41:38 +0000

    Added .zshrc link. Removed git clone lines.

--- install.sh
+++ install.sh
@@ -3,24 +3,10 @@
 # Exit on error.
 set -e
 
-default_dir=~/dotfiles
-repo="https://github.com/renyard/dotfiles.git"
+script=$(readlink -f $0)
+df_dir=$(dirname "$script")
 bashrc="~/.bashrc"
 vimrc="~/.vimrc"
-
-# Prompt for install location.
-read -e -p "Install path: " -i $default_dir df_dir
-
-# Create install location if it doesn't exist.
-if [ ! -d $df_dir ]; then
-	echo "Creating $df_dir..."
-	mkdir -p $df_dir
-fi
-
-echo 'Cloning Git Repo...'
-git clone $repo $df_dir
-
-cd $df_dir
 
 echo 'Initializing Submodules...'
 git submodule init
@@ -38,10 +24,10 @@ if [ ! -e ~/.zshrc ]; then
 	touch ~/.zshrc
 fi
 
-# Add source lines to .bashrc
-echo 'Linking .bashrc...'
+# Add source lines to .zshrc.
+echo 'Linking .zshrc...'
 zshrc="if [ -f $df_dir ]; then . $df_dir/zshrc fi";
-if ! grep -Fxq "$zshrc" ~/.zsh; then
+if ! grep -Fxq "$zshrc" ~/.zshrc; then
 	echo $zshrc >> ~/.zshrc
 fi
 
@@ -53,7 +39,7 @@ if [ ! -e ~/.bashrc ]; then
 	touch ~/.bashrc
 fi
 
-# Add source lines to .bashrc
+# Add source lines to .bashrc.
 echo 'Linking .bashrc...'
 bashrc="if [ -f $df_dir ]; then . $df_dir/bashrc fi";
 if ! grep -Fxq "$bashrc" ~/.bashrc; then
